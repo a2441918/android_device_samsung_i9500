5455334f3b4f6149c690f148342dba0985b260c9
Andreas Schneider <asn@cryptomilk.org>
2015-09-03 20:59:44 +0200

    i9500: gps: Add supl TLS support to gps config

--- i9500.mk
+++ i9500.mk
@@ -88,7 +88,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/gps/gps.cer:system/etc/gps.cer \
 	$(LOCAL_PATH)/configs/gps/gps.conf:system/etc/gps.conf \
-	$(LOCAL_PATH)/configs/gps/gps.xml:system/etc/gps.xml
+	$(LOCAL_PATH)/configs/gps/gps.xml:system/etc/gps.xml \
+	$(LOCAL_PATH)/configs/gps/SuplRootCert:system/etc/SuplRootCert
 
 # HW Composer
 PRODUCT_PACKAGES += \
