b3cbb29c082fc1760f93b8e255f6c9659f72568e
Arun Manohar Manoharan <email2arunmanohar@gmail.com>
2015-10-03 19:44:24 +0200

    i9500: add latest GearCM 12.1 updates

--- hal/lights/lights.c
+++ hal/lights/lights.c
@@ -279,12 +279,12 @@ static int open_lights(const struct hw_module_t *module, const char *name,
         set_light = set_light_backlight;
     else if (0 == strcmp(LIGHT_ID_BUTTONS, name))
         set_light = set_light_buttons;
+    else if (0 == strcmp(LIGHT_ID_BATTERY, name))
+        set_light = set_light_battery;
     else if (0 == strcmp(LIGHT_ID_NOTIFICATIONS, name))
         set_light = set_light_leds_notifications;
     else if (0 == strcmp(LIGHT_ID_ATTENTION, name))
         set_light = set_light_leds_attention;
-    else if (0 == strcmp(LIGHT_ID_BATTERY, name))
-        set_light = set_light_battery;
     else
         return -EINVAL;
 
